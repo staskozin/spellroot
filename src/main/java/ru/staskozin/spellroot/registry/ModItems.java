79716573da3195b1b38829bc9cdfbaba4c99bea1
Stanislav Kozin <stas@staskozin.ru>
2026-07-16 02:10:26 +0300

    feat(ender-focus): настроен предмет и удалён шаблонный контент

--- src/main/java/ru/staskozin/spellroot/registry/ModItems.java
+++ src/main/java/ru/staskozin/spellroot/registry/ModItems.java
@@ -1,7 +1,10 @@
 package ru.staskozin.spellroot.registry;
 
+import net.minecraft.core.component.DataComponents;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraft.world.item.Item;
 import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.event.BuildCreativeModeTabContentsEvent;
 import net.neoforged.neoforge.registries.DeferredItem;
 import net.neoforged.neoforge.registries.DeferredRegister;
 import ru.staskozin.spellroot.Spellroot;
@@ -9,12 +12,21 @@ import ru.staskozin.spellroot.Spellroot;
 public final class ModItems {
     private static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(Spellroot.MODID);
 
-    public static final DeferredItem<Item> ENDER_FOCUS = ITEMS.registerSimpleItem("ender_focus");
+    public static final DeferredItem<Item> ENDER_FOCUS = ITEMS.registerSimpleItem("ender_focus", properties -> properties
+            .stacksTo(1)
+            .component(DataComponents.ENCHANTMENT_GLINT_OVERRIDE, true));
 
     private ModItems() {
     }
 
     public static void register(IEventBus modEventBus) {
         ITEMS.register(modEventBus);
+        modEventBus.addListener(ModItems::addCreativeTabContents);
+    }
+
+    private static void addCreativeTabContents(BuildCreativeModeTabContentsEvent event) {
+        if (event.getTabKey() == CreativeModeTabs.TOOLS_AND_UTILITIES) {
+            event.accept(ENDER_FOCUS);
+        }
     }
 }
